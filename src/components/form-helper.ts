74f0d59c010b8dbb44c67abdb0108503d1c705a0
Tim Wang <tianxun.wang1@nhs.net>
2024-09-03 14:23:47 +0100

    fixed linting 2

--- src/components/form-helper.ts
+++ src/components/form-helper.ts
@@ -1,4 +1,4 @@
-import {FieldErrors, FieldValues, UseFormRegister, FieldError} from 'react-hook-form';
+import {FieldErrors, FieldValues, UseFormRegister} from 'react-hook-form';
 
 function getFormErrorMessage<T extends FieldValues>(
   formField: Parameters<UseFormRegister<T>>['0'],
